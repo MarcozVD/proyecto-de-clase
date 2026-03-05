5459100e33159fb4da73fc206a6556da04aa3f80
MarcozVD <mvaleradaza@gmail.com>
2026-03-04 22:24:00 -0500

    migraciones

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,24 +2,21 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <title>@yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title', 'TIENDA AZUL')</title>
+    
+    <!-- Estilos CSS -->
     <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
 </head>
 <body>
 
     @include('layouts.navbar')
 
+    <!-- CONTENIDO -->
     <div class="container">
         @if($message = Session::get('success'))
             <div class="alert alert-success">
                 ✓ {{ $message }}
-            </div>
-        @endif
-
-        @if($message = Session::get('error'))
-            <div class="alert alert-danger">
-                ✗ {{ $message }}
             </div>
         @endif
 
