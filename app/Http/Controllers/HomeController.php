f0b9cbc0ad6f0218d095dc3e5c98e3d770336598
MarcozVD <mvaleradaza@gmail.com>
2026-03-12 18:33:45 -0500

    En l host personal quiero que me creen una landing relacionada con el ecommers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Category;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return view('welcome');
+        $featuredProducts = Product::latest()->take(3)->get();
+        $categories = Category::limit(20)->get();
+        
+        return view('welcome', compact('featuredProducts', 'categories'));
     }
-    
 }
